34a0d7843089f2707dc381bb58ec40b14db6d85e
raunak mankapure <113294200+raunakmankapure@users.noreply.github.com>
2023-08-21 23:38:36 +0530

    SmallBot_assistant.py

--- Voice_assistant.py
+++ Voice_assistant.py
@@ -7,3 +7,5 @@ result = wikipedia.summary(In, sentences = 3)
 print(result)
 voice.say(result)
 voice.runAndWait()
+
+#before running the code in your machine , install pyttsx3 & wikipedia libraries from CLI with commands pip install pyttsx3 & pip install wikipedia respectivly 
